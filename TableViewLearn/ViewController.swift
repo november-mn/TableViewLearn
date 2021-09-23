0dd2d04b15c59a3f81c87b7eac5cd4de9d4a695b
Menghui Wang <wang.mengh@northeastern.edu>
2021-09-23 16:18:28 -0700

    NEW Intial project with runnable tableview app showing fruits

--- TableViewLearn/ViewController.swift
+++ TableViewLearn/ViewController.swift
@@ -7,11 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    let arr = ["apple", "banana", "grape", "blueberry", "strawberry", "orange"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        
+        cell.imgView.image = UIImage(named: arr[indexPath.row])
+        cell.lblImage.text = arr[indexPath.row]
+        print(indexPath.row)
+        print(arr[indexPath.row])
+        return cell;
     }
 
 
